af522d3c6ebb45b54b5364d3e593e9a0fc331db2
Carlo Cervellin <carlocervellin@gmail.com>
2018-11-01 18:06:00 +0100

    Updates

---
+++ precache-manifest.41ec6775c17d52f9ba7aaa32aceb3941.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bc16563ffa3ae5b4958",
+    "url": "/react-simple-weather/static/js/runtime~main.5bc16563.js"
+  },
+  {
+    "revision": "0b901449b36027d304ee",
+    "url": "/react-simple-weather/static/js/main.0b901449.chunk.js"
+  },
+  {
+    "revision": "e1ac019b792fc4bc4438",
+    "url": "/react-simple-weather/static/js/1.e1ac019b.chunk.js"
+  },
+  {
+    "revision": "0b901449b36027d304ee",
+    "url": "/react-simple-weather/static/css/main.658e777a.chunk.css"
+  },
+  {
+    "revision": "043eba59c46c2820ad309b67b8ee0010",
+    "url": "/react-simple-weather/index.html"
+  }
+];
